54995fbdced86fe2aca6a79dd35e38ed1334f951
Dimitar <daghostman.dd@gmail.com>
2017-09-11 04:02:00 +0300

    Include Plain Event Target

---
+++ src/Framework/Event/NullTarget.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Framework\Event;
+
+use Onion\Framework\Event\Interfaces\EventTargetInterface;
+
+class NullTarget implements EventTargetInterface
+{
+
+}
